03293acac94e49e2f5160e61cb0da57251983398
ChathurangaSN <sachithmaths@gmail.com>
2019-06-16 23:07:08 +0530

    work on controller

--- java-brain-course-api/src/main/java/com/sachith/controller/TopicController.java
+++ java-brain-course-api/src/main/java/com/sachith/controller/TopicController.java
@@ -1,13 +1,24 @@
 package com.sachith.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sachith.model.Topic;
+
 @RestController
 public class TopicController {
-	@RequestMapping("/topic")
-	public String getAllTopics() {
-		return "Get All Topoics" ;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		
+		return Arrays.asList(
+				new Topic("spring", "Spring Framework", "Spring Framework decription"),
+				new Topic("java", "Core java", "Core java decription"),
+				new Topic("javaScript", "JavaScript", "JavaScript decription")
+				);
 	}
 
 }
--- java-brain-course-api/src/main/java/com/sachith/model/Topic.java
+++ java-brain-course-api/src/main/java/com/sachith/model/Topic.java
@@ -6,6 +6,14 @@ public class Topic {
 	private String name;
 	private String description;
 	
+	public Topic() {
+	}
+	public Topic(String id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
 	
 	public String getId() {
 		return id;
